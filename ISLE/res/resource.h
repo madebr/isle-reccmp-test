33eb10941d591f5244b56ca2ea558009866c12e2
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-10-17 00:40:18 +0200

    Remove all non-python recompilation tools

--- ISLE/res/resource.h
+++
@@ -1,5 +0,0 @@
-#define ISLE_ARROW 102
-#define ISLE_NO 103
-#define ISLE_BUSY 104
-
-#define APP_ICON 105
--- LEGO1/omni/src/common/mxvariable.cpp
+++
@@ -1,6 +0,0 @@
-#include "mxvariable.h"
-
-#include "decomp.h"
-#include "mxstring.h"
-
-DECOMP_SIZE_ASSERT(MxVariable, 0x24)
